5f9bc0fbeb3c0f6d50137b7d994cd04ee8ff69a5
fg <xgaran@gmail.com>
2024-03-22 05:15:34 -0400

    fix: find atomic number only for numeric values

--- element.sh
+++ element.sh
@@ -5,19 +5,21 @@ then
     echo "Please provide an element as an argument."
 else
     # search by atomic number
-    echo "SELECT atomic_number FROM elements WHERE atomic_number = $1"
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+    if [[ $1 =~ ^[0-9]+$ ]]
+    then
+         ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+    fi 
 
     if [[ -z $ATOMIC_NUMBER ]]
     then
-    # search by symbol
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1'")
+        # search by symbol
+        ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$1'")
     fi
 
     if [[ -z $ATOMIC_NUMBER ]]
     then
-    # search by name
-    ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$1'")
+        # search by name
+        ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$1'")
     fi
 
     if [[ -z $ATOMIC_NUMBER ]]
@@ -25,17 +27,17 @@ else
     # Not found
         echo "I could not find that element in the database."
     else
-    # get elements data
-    ELEMENTS_DATA=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
-    echo $ELEMENTS_DATA
-    # get properties data
-    PROPERTIES_DATA=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
-    echo $PROPERTIES_DATA
-    # get types data
-    TYPE_ID=1
-    TYPES_DATA=$($PSQL "SELECT type_id, type FROM types WHERE type_id = $TYPE_ID")
-    echo $TYPES_DATA
-    # display properties
+        # get elements data
+        ELEMENTS_DATA=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $ATOMIC_NUMBER")
+        echo $ELEMENTS_DATA
+        # get properties data
+        PROPERTIES_DATA=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number = $ATOMIC_NUMBER")
+        echo $PROPERTIES_DATA
+        # get types data
+        TYPE_ID=1
+        TYPES_DATA=$($PSQL "SELECT type_id, type FROM types WHERE type_id = $TYPE_ID")
+        echo $TYPES_DATA
+        # display properties
         echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
     fi
 fi
